9645eb8cc377d6f95a8e8f5c0c5b4d17e931341e
JulioLaz <julioalazarte@gmail.com>
2022-10-13 23:11:41 -0300

    agregando proyectos: ahorcadito y encriptador

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-proyectos',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectosComponent implements OnInit {
 
-  constructor() { }
+  educacionList: any;
+  miPortfolio: any;
+  frases: any;
+  proyectos:any;
 
+  constructor(private datosPortfolio:PortfolioService) { }
   ngOnInit(): void {
+  this.datosPortfolio.obtenerDatos().subscribe((data: any) =>{
+    this.educacionList=data.education;
+    this.miPortfolio=data;
+    this.frases=data.frases;
+    this.proyectos=data.proyectos
+  });
   }
 
 }
