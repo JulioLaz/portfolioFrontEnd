49e82578fd1580653215d017ec3afa3f1fe6c342
JulioLaz <julioalazarte@gmail.com>
2022-10-18 15:55:32 -0300

    mejoras en cards

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
-
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
@@ -10,15 +9,23 @@ export class EducacionComponent implements OnInit {
   educacionList: any;
   miPortfolio: any;
   frases: any;
+  id!:number;
   deleteCards:boolean=true;
+  currentYear=new Date();
+
 
   constructor(private datosPortfolio:PortfolioService) { }
   ngOnInit(): void {
-  this.datosPortfolio.obtenerDatos().subscribe((data: any) =>{
-    this.educacionList=data.education;
-    this.miPortfolio=data;
-    this.frases=data.frases;
-  });
+    // this.currentYear=new Date("YYYY");
+    console.log(this.currentYear);
+
+    this.datosPortfolio.obtenerDatos().subscribe((data: any) =>{
+      this.educacionList=data.education;
+      this.miPortfolio=data;
+      this.frases=data.frases;
+      this.id=data.educacion.id;
+      console.log(data.educacion.id);
+    });
 
   }
 }
