c7fb94be3a3f861b0f571284422a9d60e779c267
JulioLaz <julioalazarte@gmail.com>
2022-10-19 00:23:46 -0300

    btn edit en cards

--- src/app/componentes/about/about.component.ts
+++ src/app/componentes/about/about.component.ts
@@ -21,7 +21,6 @@ textoOriginal(){
   this.newMiHistoria = this.historiaOriginal;
 }
 
-
 cerrar(){
   this.newMiHistoria = this.miHistoria;
 }
--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -12,19 +12,39 @@ export class EducacionComponent implements OnInit {
   id!:number;
   deleteCards:boolean=true;
   currentYear=new Date();
+  editCards:boolean=true;
+  fondo:boolean=false;
+  miSchool:any;
+  newMiSchool:any;
 
+  checkFondo(){
+    this.fondo=!this.fondo;
+    }
 
+  asignaClases(){
+    let classes = {
+    bg_edit_card: this.fondo,
+    };
+    return classes;
+    }
+
+    save(){
+      // this.miSchool=this.newMiSchool;
+      this.newMiSchool=this.miSchool;
+      console.log(this.miSchool)
+    }
   constructor(private datosPortfolio:PortfolioService) { }
   ngOnInit(): void {
-    // this.currentYear=new Date("YYYY");
-    console.log(this.currentYear);
 
     this.datosPortfolio.obtenerDatos().subscribe((data: any) =>{
       this.educacionList=data.education;
       this.miPortfolio=data;
       this.frases=data.frases;
       this.id=data.educacion.id;
-      console.log(data.educacion.id);
+      this.miSchool=data.education.school;
+
+
+      // console.log(data.education.school);
     });
 
   }
