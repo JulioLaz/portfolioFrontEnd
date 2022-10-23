0f13711e06221a59c3c7d87520e80338e7ddbaec
JulioLaz <julioalazarte@gmail.com>
2022-10-22 23:59:55 -0300

    edit experiencia

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -11,6 +11,32 @@ export class ExperienciaComponent implements OnInit {
  name:any;
  frases:any;
 
+ educacionList: any;
+ miPortfolio: any;
+ id:any=0;
+ idCard:number=0;
+ deleteCards:boolean=true;
+ currentYear=new Date();
+ editCards:boolean=true;
+ editCard:boolean=true;
+ borrar:boolean=true;
+ editCardItem:any=false;
+ fondo:boolean=false;
+ miSchool:any;
+ newMiSchool:any;
+ disabledFlag:boolean=false;
+
+
+  checkFondo(){
+    this.fondo=!this.fondo;
+    }
+
+  asignaClases(){
+    let classes = {
+    bg_edit_card: this.fondo,
+    };
+    return classes;
+    }
   constructor(private datosPortfolio:PortfolioService) { }
   ngOnInit(): void {
   this.datosPortfolio.obtenerDatos().subscribe((data: any) =>{
